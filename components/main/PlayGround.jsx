99ba76fc01bada1af769799f8accb8ae218f6cd5
Raúl Camacho <raulj.camacho@gmail.com>
2023-03-05 21:06:58 -0500

    Add: Array History and fixed env archive

--- components/main/PlayGround.jsx
+++ components/main/PlayGround.jsx
@@ -1,29 +1,35 @@
 import styles from '../../styles/Home.module.css';
-import { Configuration, OpenAIApi } from 'openai';
-// import { createCompletion } from '../../pages/api/chatcompletions';
+
+import { createCompletion } from '../../pages/api/chatcompletions';
 import { useState } from 'react';
 import { useRef } from 'react';
 
 export const PlayGround = () => {
   const ref = useRef(null);
+
+  const arryMsgs = [];
+
   const [result, setResult] = useState('');
   const [prompt, setPrompt] = useState('');
   const [isPrompt, setIsPrompt] = useState(false);
+  const [historyMsg, setHistoryMsg] = useState(arryMsgs);
 
-  const configuration = new Configuration({
-    apiKey: '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',
-  });
-  const openai = new OpenAIApi(configuration);
+  console.log(historyMsg);
 
-  const createCompletion = async () => {
+  const completion = async () => {
     ref.current.value = '';
     setIsPrompt(true);
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: prompt }],
-    });
-    console.log(response.data.choices[0].message.content);
-    setResult(response.data.choices[0].message.content);
+    setResult('');
+    const response = await createCompletion(prompt);
+    setResult(response);
+    setHistoryMsg([
+      ...historyMsg,
+      {
+        userMsg: prompt,
+        AiMsg: response,
+      },
+    ]);
+    setIsPrompt(false);
   };
 
   const handleChange = event => {
@@ -33,39 +39,70 @@ export const PlayGround = () => {
   return (
     <div className="mx-auto w-5/6 h-screen">
       <div className={styles.playgroundcontainer}>
-        <div className=" flex flex-row-reverse bg-gray-50  h-full rounded-lg border-gray-200  p-12 text-sm shadow-sm">
-          {isPrompt ? (
-            <>
-              <div className="w-2/4 pl-2">
-                <div className="flex justify-start bg-cyan-100 p-2">
-                  <p className="text-2xl">Raúl</p>
-                </div>
-                <div className="bg-white p-2">
-                  <p>{prompt}</p>
-                </div>
-              </div>
-              <div className="w-2/4 mt-20 pr-2">
-                <div className="flex bg-fuchsia-100 p-2">
-                  <p className="text-2xl">Chat Diftinto</p>
-                </div>
-                {result.length > 0 ? (
-                  <>
-                    <div className="bg-white p-2">
-                      <p>{result}</p>
+        {
+          historyMsg.length > 0
+            ? historyMsg.map(msg => (
+                <div
+                  id={msg.AiMsg}
+                  className=" flex flex-row-reverse  bg-gray-100  h-full rounded-lg border-gray-200 py-4 px-6  text-sm shadow-sm">
+                  <div className="w-2/4 pl-2">
+                    <div className="flex justify-start bg-cyan-100 p-2 rounded-t-lg">
+                      <p className="text-2xl">Raúl</p>
                     </div>
-                  </>
-                ) : (
-                  <div className="flex flex-row bg-white p-2">
-                    <p>Escribiendo &nbsp; </p>
-                    <img src="/loading.gif" alt="..." width="20" />
+                    <div className="bg-white p-2 rounded-b-lg">
+                      <p>{msg.userMsg}</p>
+                    </div>
                   </div>
-                )}
+
+                  <div className="w-2/4 mt-20 pr-2 ">
+                    <div className="flex bg-fuchsia-100 p-2 rounded-t-lg">
+                      <p className="text-2xl">Chat Diftinto</p>
+                    </div>
+                    <div className="bg-white p-2 rounded-b-lg">
+                      <p>{msg.AiMsg}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : ''
+          // <div className="flex bg-gray-50   h-full rounded-lg border-gray-200 py-4 px-6  text-sm shadow-sm">
+          //   <div className="flex flex-row bg-white p-2">
+          //     <p>Escribiendo &nbsp; </p>
+          //     <img src="/loading.gif" alt="..." width="20" />
+          //   </div>
+          // </div>
+        }
+        {isPrompt ? (
+          <div className=" flex flex-row-reverse  bg-gray-100  h-full rounded-lg border-gray-200 py-4 px-6  text-sm shadow-sm">
+            <div className="w-2/4 pl-2">
+              <div className="flex justify-start bg-cyan-100 p-2 rounded-t-lg">
+                <p className="text-2xl">Raúl</p>
               </div>
-            </>
-          ) : (
-            ''
-          )}
-        </div>
+              <div className="bg-white p-2">
+                <p>{prompt}</p>
+              </div>
+            </div>
+            <div className="w-2/4 mt-20 pr-2">
+              <div className="flex bg-fuchsia-100 p-2 rounded-t-lg">
+                <p className="text-2xl">Chat Diftinto</p>
+              </div>
+              {result.length > 0 ? (
+                <>
+                  <div className="bg-white p-2 rounded-b-lg">
+                    <p>{result}</p>
+                  </div>
+                </>
+              ) : (
+                <div className="flex flex-row bg-white p-2 rounded-b-lg">
+                  <p>Escribiendo &nbsp; </p>
+                  <img src="/loading.gif" alt="..." width="20" />
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
       <div>
         <div className="relative">
@@ -73,7 +110,7 @@ export const PlayGround = () => {
             id="prompt"
             ref={ref}
             name="prompt"
-            className=" bg-gray-50 w-full rounded-lg border-gray-200 my-6 p-4 pr-12 text-sm shadow-sm"
+            className=" bg-gray-100 w-full rounded-lg border-gray-200 my-6 p-4 pr-12 text-sm shadow-sm"
             onChange={handleChange}
           />
         </div>
@@ -81,7 +118,7 @@ export const PlayGround = () => {
       <div className="flex items-center justify-between">
         <button
           className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-          onClick={() => createCompletion()}>
+          onClick={() => completion()}>
           Enviar
         </button>
       </div>
--- pages/api/chatcompletions.js
+++ pages/api/chatcompletions.js
@@ -1,14 +1,15 @@
 import { Configuration, OpenAIApi } from 'openai';
 
+const apikeyOpenAi = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: apikeyOpenAi,
 });
 const openai = new OpenAIApi(configuration);
 
-export const createCompletion = async () => {
+export const createCompletion = async prompt => {
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content: 'Hello world' }],
+    messages: [{ role: 'user', content: prompt }],
   });
   console.log(completion.data.choices[0].message.content);
   return completion.data.choices[0].message.content;
